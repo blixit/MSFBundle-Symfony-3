382dd2baf7c6cbb86462dccce477c33b5eaee049
blixit <celestinya@gmail.com>
2017-05-30 23:32:08 +0200

    Achieving to test EventListeners and MSFDataloader

---
+++ Tests/EventListener/MSFRedirectExceptionListenerTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blixit
+ * Date: 30/05/17
+ * Time: 23:13
+ */
+
+namespace Blixit\MSFBundle\Tests\EventListener;
+
+use Blixit\MSFBundle\EventListener\MSFRedirectExceptionListener;
+
+class MSFRedirectExceptionListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
